060f5ba503d35b1ad079c5132472163e55d03f41
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-05-23 22:51:56 +0530

    devise config

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem 'letter_opener'
 end
 
 group :test do
@@ -72,3 +73,5 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
+gem 'devise'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,10 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  # letter opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -40,7 +44,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
